59cc9115e62330e04d2779e5e69048268cc9e30d
skudryavtsev <s2.kudryavtsev@itransition.com>
2019-02-14 19:27:41 +0300

    add material-ui

--- src/components/forms/loginForm.tsx
+++ src/components/forms/loginForm.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
 
 export const LoginForm = (): JSX.Element => {
     return (
         <div>
             LoginForm
+            <form>
+                <TextField
+                    className = "text-primary"
+                    id="standard-name"
+                    label="Name"
+                    // className={classes.textField}
+                    // value={this.state.name}
+                    // onChange={this.handleChange('name')}
+                    margin="normal"
+                />
+            </form>
             {/* <Link to="/signin">Регистрация</Link> */}
         </div>
     );
