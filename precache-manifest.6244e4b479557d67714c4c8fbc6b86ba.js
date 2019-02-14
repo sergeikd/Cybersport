922dd36a235462f95dfef1211528d0b3d83de490
sergeikd <sergeikd@gmail.com>
2019-02-15 00:07:54 +0300

    build

---
+++ precache-manifest.6244e4b479557d67714c4c8fbc6b86ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "63603d1b500031009e6a355dbfe0f18a",
+    "url": "/Cybersport/static/media/logo.63603d1b.jpg"
+  },
+  {
+    "revision": "e81f6311a7f1c1807dc0c4c47d6642ea",
+    "url": "/Cybersport/static/media/background.e81f6311.jpg"
+  },
+  {
+    "revision": "e0f1089fa34244941459",
+    "url": "/Cybersport/static/js/runtime~main.e0f1089f.js"
+  },
+  {
+    "revision": "6d2ab59431bd32d8d6c6",
+    "url": "/Cybersport/static/js/main.6d2ab594.chunk.js"
+  },
+  {
+    "revision": "7947d3c94e708a4e2b35",
+    "url": "/Cybersport/static/js/1.7947d3c9.chunk.js"
+  },
+  {
+    "revision": "6d2ab59431bd32d8d6c6",
+    "url": "/Cybersport/static/css/main.4896602e.chunk.css"
+  },
+  {
+    "revision": "fef06b1c90e4d9fd073cdf939e4273c8",
+    "url": "/Cybersport/index.html"
+  }
+];
